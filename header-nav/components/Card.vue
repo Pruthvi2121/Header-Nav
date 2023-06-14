f2f2fc0e30c1eb7449ca81a0029d53d599932f79
Pruthviraj <chokake.pruthvi@gmail.com>
2023-06-14 19:01:37 +0530

    card component

---
+++ header-nav/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="border h-64  justify-between w-64 mx-auto rounded bg-[#ec78db25] flex flex-col">
+        <img class="h-[60%]" src="static/img2.jpg" alt=""/>
+        <div class="flex flex-col items-center ">
+            <h1 class="font-semibold text-[#06669a]">Web development ..</h1>
+            <p class="text-slate-400 text-center ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, ullam?</p>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default{
+    props:['img']
+};
+
+</script>
--- header-nav/pages/index.vue
+++ header-nav/pages/index.vue
@@ -2,6 +2,14 @@
     <div class="text-center text-4xl mt-16 font-bold">
         <h1>THis is home page ..</h1>
     </div>
+
+    <div class="my-8 grid grid-rows-2 grid-flow-col gap-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+
+    </div>
 </template>
 
 <script>
